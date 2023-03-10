1f458892a51aa1896500bfe0d588667f8dbbbe3d
oyugibear <65049957+oyugibear@users.noreply.github.com>
2023-03-10 10:16:32 +0300

    added the rest of the forms

--- components/Forms/CorporateForm.jsx
+++ components/Forms/CorporateForm.jsx
@@ -1,23 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BiChevronRight} from 'react-icons/bi'
 import { toast } from 'react-toastify'
+import { DatePicker } from 'antd';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import axios from 'axios';
+import { API_URL } from '../../config/api.config';
+dayjs.extend(customParseFormat);
 
 const CorporateForm = () => {
-  const handleSubmit = (e) => {
+  const [name, setName] = useState('')
+  const [companyName, setCompanyName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [parterType, setPartnerType] = useState('')
+  const [employees, setEmployees] = useState('')
+  const [date, setDate] = useState('')
+
+  const format = 'HH:mm';
+  const dateFormat = 'DD/MM/YYYY';
+
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+    setDate(dateString)
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    toast.success("Form submitted")
+    try {
+      let details = {
+        name: name,
+        phone_no: number,
+        company_name: companyName,
+        email: email,
+        number_of_employees: employees,
+        sample_collection_date: date,
+      }
+      const {data} = await axios.post(
+        `${API_URL}/web_services/coporate_service_request`,
+        details 
+      )
+      if(data){
+        toast.success("Form submitted, an email has been sent to you")
+      }
+    } catch (error) {
+      toast.error("An error occured uploading your form")
+      console.log(error)
+    }
   }
+
   return (
     <div className='bg-gray-200 flex flex-col border mt-12 px-8 md:px-6 lg:px-12 py-6 rounded-3xl'>
         <h2 className='text-2xl font-bold mb-4 pt-4'>Get Quote Below</h2>
         <p>Please ﬁll in the form below to start</p>
 
         <form className='my-4' onSubmit={(e) => handleSubmit(e)}>
-            <input type="text" placeholder='Name' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="number" placeholder='Contact Number' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="text" placeholder='Company Name' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="number" placeholder='Number Of Employees' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="text" placeholder='Preferred Sample Collection Date' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="tel" value={number} onChange={(e) => setNumber(e.target.value)} placeholder='Contact Number' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)} placeholder='Company Name' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="number" value={employees} onChange={(e) => setEmployees(e.target.value)} placeholder='Number Of Employees' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' className='border-2 w-full p-4 rounded-xl my-2' />
+            <DatePicker placeholder='Preferred Sample Collection Date' onChange={onChange} format={dateFormat} className='border-2 w-full p-4 rounded-xl my-2'/>
 
             <button type='submit' className='w-full bg-[#FCB7AC] text-white p-4 rounded-xl my-4 flex flex-row items-center justify-between'>
                 <p className='font-bold '>Submit Information Now</p>
--- components/Forms/PartnerForm.jsx
+++ components/Forms/PartnerForm.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import {BiChevronRight} from 'react-icons/bi'
 import { toast } from 'react-toastify'
+import { API_URL } from '../../config/api.config'
 
 const PartnerForm = () => {
-  const handleSubmit = (e) => {
+  
+  const [name, setName] = useState('')
+  const [companyName, setCompanyName] = useState('')
+  const [email, setEmail] = useState('')
+  const [number, setNumber] = useState('')
+  const [parterType, setPartnerType] = useState('')
+  const [country, setCountry] = useState('')
+
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    toast.success("Form submitted")
+    try {
+      let details = {
+        name: name,
+        contact_number: number,
+        email: email,
+        partner_type: parterType,
+        country: country,
+      }
+      const {data} = await axios.post(
+        `${API_URL}/web_services/become_a_partner`,
+        details 
+      )
+      if(data){
+        toast.success("Form submitted, an email has been sent to you")
+      }
+    } catch (error) {
+      toast.error("An error occured uploading your form")
+      console.log(error)
+    }
   }
   return (
     <div className='bg-gray-200 flex flex-col border px-6 md:px-12 py-6 md:w-[35vw] lg:w-[25vw] rounded-3xl'>
         <h2 className='text-2xl font-bold mb-4 pt-4'>Become a Partner</h2>
         <p>Please ﬁll in the form below to join</p>
 
-        <form className='my-4'>
-            <input type="text" placeholder='Name' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="number" placeholder='Contact Number' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="email" placeholder='Email Address' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="text" placeholder='Partner as' className='border-2 w-full p-4 rounded-xl my-2' />
-            <input type="text" placeholder='Country Selection' className='border-2 w-full p-4 rounded-xl my-2' />
+        <form className='my-4' onSubmit={(e) => handleSubmit(e)}>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Name' className='border-2 w-full p-4 rounded-xl my-2' />
+            {/* <input type="text" value={companyName} onChange={(e) => setCompanyName(e.target.value)} placeholder='Company Name' className='border-2 w-full p-4 rounded-xl my-2' /> */}
+            <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} placeholder='Contact Number' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email Address' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="text" value={parterType} onChange={(e) => setPartnerType(e.target.value)} placeholder='Partner as' className='border-2 w-full p-4 rounded-xl my-2' />
+            <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder='Country Selection' className='border-2 w-full p-4 rounded-xl my-2' />
 
-            <button type='submit' onSubmit={(e) => handleSubmit(e)} className='w-full bg-[#052749] text-white p-4 rounded-xl my-4 flex flex-row items-center justify-between'>
+            <button type='submit'  className='w-full bg-[#052749] text-white p-4 rounded-xl my-4 flex flex-row items-center justify-between'>
                 <p className='font-bold '>Submit Information Now</p>
                 <BiChevronRight />
             </button>
